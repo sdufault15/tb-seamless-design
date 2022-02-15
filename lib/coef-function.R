4fe2878231e342da0e3584c0371a20f1c5ec8246
Suzanne Dufault <sdufault@dom-7g8ml7h-lt.ucsf.edu>
2022-02-15 12:54:25 -0800

    coef function corrected

--- lib/coef-function.R
+++ lib/coef-function.R
@@ -14,11 +14,12 @@ coef.function <- function(s.1, s.k, t.1 = 26, d, max.d.k, p = 0.425, duration.ef
 
   lhs <- log(k.lambda/ref.lambda)
 
-  main.eff <- ifelse(d == max.d.k, lhs, lhs/(duration.effect*(max.d.k - d)))
+  main.eff <- ifelse(d == max.d.k, lhs, 
+                     lhs - (duration.effect*(max.d.k - d)))
 
   output <- data.frame(ref.lambda = ref.lambda,
                        k.lambda = k.lambda,
-                       logHR = log(k.lambda/ref.lambda),
+                       logHR = lhs,
                        gfun = max.d.k - d,
                        main.eff = main.eff)
   return(output)
