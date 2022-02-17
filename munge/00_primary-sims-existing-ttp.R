25babf03a12c3cb42217cf0af977fa2903c076f0
Suzanne Dufault <sdufault@dom-7g8ml7h-lt.ucsf.edu>
2022-02-17 14:29:43 -0800

    updating for running on cluster

--- munge/00_primary-sims-existing-ttp.R
+++ munge/00_primary-sims-existing-ttp.R
@@ -5,3 +5,4 @@ library(here)
 library(tidyverse)
 
 source(here("munge","01_nk-30-fixed-effects.R"))
+source(here("munge","02_nk-30-survival-outcomes.R")) # this takes a few hours to run - would be better on cluster
