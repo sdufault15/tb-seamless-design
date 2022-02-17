1e25e884172be13dad3fec128682ccc072d68118
Suzanne Dufault <sdufault@dom-7g8ml7h-lt.ucsf.edu>
2022-02-17 09:58:07 -0800

    starting to simulate survival effects

---
+++ munge/02_nk-30-survival-outcomes.R
@@ -0,0 +1,4 @@
+###########################################################################
+# Call in survival characteristics and generate survival outcomes
+# Separate file per sample size
+###########################################################################
