88b8a9d8b3588d4ef685063cc1572cf6ef60ff68
Suzanne Dufault <sdufault15@gmail.com>
2022-03-03 09:37:49 -0800

    fixed file path

--- munge/32_analyze-ttp-slope/30_job-file-for-running-on-cluster.R
+++ munge/32_analyze-ttp-slope/30_job-file-for-running-on-cluster.R
@@ -7,6 +7,6 @@
 library(here)
 
 sims <- c(1:200)
-source(here("munge","33_CLUSTER-brms-models-on-sim-data_even-condition.R"))
-source(here("munge","34_CLUSTER-brms-models-on-sim-data_clustered-high-condition.R"))
-source(here("munge","35_CLUSTER-brms-models-on-sim-data_clustered-highlow-condition.R"))
+source(here("munge","32_analyze-ttp-slope","33_CLUSTER-brms-models-on-sim-data_even-condition.R"))
+source(here("munge","32_analyze-ttp-slope","34_CLUSTER-brms-models-on-sim-data_clustered-high-condition.R"))
+source(here("munge","32_analyze-ttp-slope","35_CLUSTER-brms-models-on-sim-data_clustered-highlow-condition.R"))
