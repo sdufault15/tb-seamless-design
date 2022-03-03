af1afa9b1374699f5c75e07a2aee6a24b4e42569
Suzanne Dufault <sdufault15@gmail.com>
2022-03-03 08:24:05 -0800

    updated for 60-80

--- munge/32_analyze-ttp-slope/35_CLUSTER-brms-models-on-sim-data_clustered-highlow-condition.R
+++ munge/32_analyze-ttp-slope/35_CLUSTER-brms-models-on-sim-data_clustered-highlow-condition.R
@@ -4,9 +4,12 @@
 library(tidyverse)
 library(parallely)
 library(brms)
-source("~/phase2b/lib/df_extract-mcmc-slopes-function.R")
+source("~/tb-seamless/lib/df_extract-mcmc-slopes-function.R")
 
-load("~/phase2b/data/2022-01-12_simulated-df_cluster-low-high.RData")
+files <- file.info(list.files(here("data", "simulated-datasets/"), full.names = T))
+file_interest <- files[stringr::str_detect(rownames(files), "simulated-df_cluster-low-high_nk60-80"),]
+load(rownames(file_interest)[which.max(file_interest$mtime)])
+rm(file_interest,files)
 
 library(rstan)
 rstan_options(auto_write = TRUE)
@@ -14,95 +17,67 @@ options(mc.cores = parallelly::availableCores())
 
 
 ######################
-# Sample Size 20
+# Sample Size 60
 ######################
 
 # Separating the simulated data by sample size
-nk_20 <- df_sims_s2[sims] %>% 
+nk_60 <- df_sims_s2[sims] %>% 
   map(~.x[[1]]) %>% 
   map(~mutate(.x, arm = as.factor(arm))) # make the arm a factor variable rather than integer
 
 rm(df_sims_s2)
 
-mods_highlow_20 <- map(nk_20,
+mods_highlow_60 <- map(nk_60,
                     ~brm(yij_censored | cens(censored) ~ 1 + week + week:arm + (1 | patient.id),
                          data = .x,
                          prior = set_prior("uniform(-1,1)", lb = -1, ub = 1, class = "b")))
 
 # Save the model results
-summary_mods_highlow_20 <- map(mods_highlow_20,
+summary_mods_highlow_60 <- map(mods_highlow_60,
                             ~summary(.x))
-save(summary_mods_highlow_20,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-20_highlow-condition_modresults-", max(sims), ".RData"))
+save(summary_mods_highlow_60,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-60_highlow-condition_modresults-", max(sims), ".RData"))
 
-rm(summary_mods_highlow_20)
+rm(summary_mods_highlow_60)
 
 # Save the MCMC chains
-mcmc_mods_highlow_20 <- map(mods_highlow_20,
+mcmc_mods_highlow_60 <- map(mods_highlow_60,
                          ~mcmc_estimates_function(.x))
-save(mcmc_mods_highlow_20,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-20_highlow-condition_mcmc-", max(sims), ".RData"))
+save(mcmc_mods_highlow_60,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-60_highlow-condition_mcmc-", max(sims), ".RData"))
 
-rm(mods_highlow_20, mcmc_mods_highlow_20, nk_20)
+rm(mods_highlow_60, mcmc_mods_highlow_60, nk_60)
 
 ######################
-# Sample Size 30
+# Sample Size 80
 ######################
 
-load("~/phase2b/data/2022-01-12_simulated-df_cluster-low-high.RData")
+files <- file.info(list.files(here("data", "simulated-datasets/"), full.names = T))
+file_interest <- files[stringr::str_detect(rownames(files), "simulated-df_cluster-low-high_nk60-80"),]
+load(rownames(file_interest)[which.max(file_interest$mtime)])
+rm(file_interest,files)
 
-nk_30 <- df_sims_s2[sims] %>% 
+nk_80 <- df_sims_s2[sims] %>% 
   map(~.x[[2]]) %>% 
   map(~mutate(.x, arm = as.factor(arm))) # make the arm a factor variable rather than integer
 
 rm(df_sims_s2)
 
-mods_highlow_30 <- map(nk_30,
+mods_highlow_80 <- map(nk_80,
                     ~brm(yij_censored | cens(censored) ~ 1 + week + week:arm + (1 | patient.id),
                          data = .x))
 # Save the model results
-summary_mods_highlow_30 <- map(mods_highlow_30,
+summary_mods_highlow_80 <- map(mods_highlow_80,
                                ~summary(.x))
-save(summary_mods_highlow_30,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-30_highlow-condition_modresults-", max(sims), ".RData"))
+save(summary_mods_highlow_80,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-80_highlow-condition_modresults-", max(sims), ".RData"))
 
-rm(summary_mods_highlow_30)
+rm(summary_mods_highlow_80)
 
 # Save the MCMC chains
-mcmc_mods_highlow_30 <- map(mods_highlow_30,
+mcmc_mods_highlow_80 <- map(mods_highlow_80,
                             ~mcmc_estimates_function(.x))
-save(mcmc_mods_highlow_30,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-30_highlow-condition_mcmc-", max(sims), ".RData"))
+save(mcmc_mods_highlow_80,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-80_highlow-condition_mcmc-", max(sims), ".RData"))
 
-rm(mods_highlow_30, mcmc_mods_highlow_30, nk_30)
-
-######################
-# Sample Size 40
-######################
-
-load("~/phase2b/data/2022-01-12_simulated-df_cluster-low-high.RData")
-
-nk_40 <- df_sims_s2[sims] %>% 
-  map(~.x[[3]]) %>% 
-  map(~mutate(.x, arm = as.factor(arm))) # make the arm a factor variable rather than integer
-
-rm(df_sims_s2)
-
-mods_highlow_40 <- map(nk_40,
-                    ~brm(yij_censored | cens(censored) ~ 1 + week + week:arm + (1 | patient.id),
-                         data = .x))
-# Save the model results
-summary_mods_highlow_40 <- map(mods_highlow_40,
-                               ~summary(.x))
-save(summary_mods_highlow_40,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-40_highlow-condition_modresults-", max(sims), ".RData"))
-
-rm(summary_mods_highlow_40)
-
-# Save the MCMC chains
-mcmc_mods_highlow_40 <- map(mods_highlow_40,
-                            ~mcmc_estimates_function(.x))
-save(mcmc_mods_highlow_40,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-40_highlow-condition_mcmc-", max(sims), ".RData"))
-
-rm(mods_highlow_40, mcmc_mods_highlow_40, nk_40)
+rm(mods_highlow_80, mcmc_mods_highlow_80, nk_80)
