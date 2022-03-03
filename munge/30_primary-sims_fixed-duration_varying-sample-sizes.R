4767fd58499ecf73e8aa0383f4d6b8b350b71834
Suzanne Dufault <sdufault15@gmail.com>
2022-03-03 08:58:43 -0800

    error in file path

--- munge/30_primary-sims_fixed-duration_varying-sample-sizes.R
+++ munge/30_primary-sims_fixed-duration_varying-sample-sizes.R
@@ -5,7 +5,7 @@ library(here)
 library(tidyverse)
 
 source(here("munge","31_simulate-data.R"))
-source(here("munge","32_analyze-ttp-slope.R", "30_job-file-for-running-on-cluster.R")) # this takes A LONG TIME
+source(here("munge","32_analyze-ttp-slope", "30_job-file-for-running-on-cluster.R")) # this takes A LONG TIME
 source(here("munge","33_nk-60-80-fixed-effects.R")) # add enrolment time
 source(here("munge","34_nk-60-survival-outcomes.R")) # adds survival to sample size of 60
 source(here("munge","34_nk-80-survival-outcomes.R")) # adds survival to sample size of 80
