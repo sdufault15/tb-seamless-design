5a977504ccc2c90b4ff7e64bbcb1444b85f5a4ac
Suzanne Dufault <sdufault15@gmail.com>
2022-03-03 08:43:53 -0800

    updates to help file flow properly

--- munge/30_primary-sims-existing-ttp_fixed-duration_varying-sample-sizes.R
+++
@@ -1,8 +0,0 @@
-###########################################################################
-# Primary file for simulating seamless design data from existing log10(TTP)
-###########################################################################
-library(here)
-library(tidyverse)
-
-source(here("munge","21_nk-30-fixed-effects_fixed-duration.R"))
-source(here("munge","22_nk-30-survival-outcomes.R")) # this takes a few hours to run - would be better on cluster
--- munge/32_analyze-ttp-slope/30_job-file-for-running-on-cluster.R
+++ munge/32_analyze-ttp-slope/30_job-file-for-running-on-cluster.R
@@ -6,7 +6,7 @@
 
 library(here)
 
-sims <- c(1:100)
+sims <- c(1:200)
 source(here("munge","33_CLUSTER-brms-models-on-sim-data_even-condition.R"))
 source(here("munge","34_CLUSTER-brms-models-on-sim-data_clustered-high-condition.R"))
 source(here("munge","35_CLUSTER-brms-models-on-sim-data_clustered-highlow-condition.R"))
