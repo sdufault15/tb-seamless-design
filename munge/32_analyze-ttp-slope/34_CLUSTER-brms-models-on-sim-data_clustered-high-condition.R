588fe65b8691d1e0559d5f6e9c93be31d68cadeb
Suzanne Dufault <sdufault15@gmail.com>
2022-03-03 08:22:23 -0800

    updated for 60-80

--- munge/32_analyze-ttp-slope/34_CLUSTER-brms-models-on-sim-data_clustered-high-condition.R
+++ munge/32_analyze-ttp-slope/34_CLUSTER-brms-models-on-sim-data_clustered-high-condition.R
@@ -4,110 +4,84 @@
 library(tidyverse)
 library(parallely)
 library(brms)
-source("~/phase2b/lib/df_extract-mcmc-slopes-function.R")
+source("~/tb-seamless/lib/df_extract-mcmc-slopes-function.R")
 
-load("~/phase2b/data/2022-01-12_simulated-df_clustered-high.RData")
+files <- file.info(list.files(here("data", "simulated-datasets/"), full.names = T))
+file_interest <- files[stringr::str_detect(rownames(files), "simulated-df_clustered-high_nk60-80"),]
+load(rownames(file_interest)[which.max(file_interest$mtime)])
+rm(file_interest,files)
 
 library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallelly::availableCores())
 
 ######################
-# Sample Size 20
+# Sample Size 60
 ######################
 
 # Separating the simulated data by sample size
-nk_20 <- df_sims_s3[sims] %>% 
+nk_60 <- df_sims_s3[sims] %>% 
   map(~.x[[1]]) %>% 
   map(~mutate(.x, arm = as.factor(arm))) # make the arm a factor variable rather than integer
 
 rm(df_sims_s3)
 
 
-mods_high_20 <- map(nk_20,
+mods_high_60 <- map(nk_60,
                     ~brm(yij_censored | cens(censored) ~ 1 + week + week:arm + (1 | patient.id),
                          data = .x,
                          prior = set_prior("uniform(-1,1)", lb = -1, ub = 1, class = "b")))
 
 # Save the model results
-summary_mods_high_20 <- map(mods_high_20,
+summary_mods_high_60 <- map(mods_high_60,
                             ~summary(.x))
-save(summary_mods_high_20,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-20_high-condition_modresults-", max(sims), ".RData"))
+save(summary_mods_high_60,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-60_high-condition_modresults-", max(sims), ".RData"))
 
-rm(summary_mods_high_20)
+rm(summary_mods_high_60)
 
 # Save the MCMC chains
-mcmc_mods_high_20 <- map(mods_high_20,
+mcmc_mods_high_60 <- map(mods_high_60,
                          ~mcmc_estimates_function(.x))
-save(mcmc_mods_high_20,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-20_high-condition_mcmc-", max(sims), ".RData"))
+save(mcmc_mods_high_60,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-60_high-condition_mcmc-", max(sims), ".RData"))
 
-rm(mods_high_20, mcmc_mods_high_20, nk_20)
+rm(mods_high_60, mcmc_mods_high_60, nk_60)
 
 
 #####################
-# Sample Size = 30
+# Sample Size = 80
 #####################
 
-load("~/phase2b/data/2022-01-12_simulated-df_clustered-high.RData")
+files <- file.info(list.files(here("data", "simulated-datasets/"), full.names = T))
+file_interest <- files[stringr::str_detect(rownames(files), "simulated-df_clustered-high_nk60-80"),]
+load(rownames(file_interest)[which.max(file_interest$mtime)])
+rm(file_interest,files)
 
-nk_30 <- df_sims_s3[sims] %>% 
+nk_80 <- df_sims_s3[sims] %>% 
   map(~.x[[2]]) %>% 
   map(~mutate(.x, arm = as.factor(arm))) # make the arm a factor variable rather than integer
 
 rm(df_sims_s3)
 
-mods_high_30 <- map(nk_30,
+mods_high_80 <- map(nk_80,
                     ~brm(yij_censored | cens(censored) ~ 1 + week + week:arm + (1 | patient.id),
                          data = .x,
                          prior = set_prior("uniform(-1,1)", lb = -1, ub = 1, class = "b")))
 
 # Save the model results
-summary_mods_high_30 <- map(mods_high_30,
+summary_mods_high_80 <- map(mods_high_80,
                             ~summary(.x))
-save(summary_mods_high_30,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-30_high-condition_modresults-", max(sims), ".RData"))
+save(summary_mods_high_80,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-80_high-condition_modresults-", max(sims), ".RData"))
 
-rm(summary_mods_high_30)
+rm(summary_mods_high_80)
 
 # Save the MCMC chains
-mcmc_mods_high_30 <- map(mods_high_30,
+mcmc_mods_high_80 <- map(mods_high_80,
                          ~mcmc_estimates_function(.x))
-save(mcmc_mods_high_30,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-30_high-condition_mcmc-", max(sims), ".RData"))
+save(mcmc_mods_high_80,
+     file = paste0("~/tb-seamless/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-80_high-condition_mcmc-", max(sims), ".RData"))
 
-rm(mods_high_30, mcmc_mods_high_30, nk_30)
+rm(mods_high_80, mcmc_mods_high_80, nk_80)
 
-#####################
-# Sample Size = 40
-#####################
-
-load("~/phase2b/data/2022-01-12_simulated-df_clustered-high.RData")
-
-nk_40 <- df_sims_s3[sims] %>% 
-  map(~.x[[3]]) %>% 
-  map(~mutate(.x, arm = as.factor(arm))) # make the arm a factor variable rather than integer
-
-rm(df_sims_s3)
-
-mods_high_40 <- map(nk_40,
-                    ~brm(yij_censored | cens(censored) ~ 1 + week + week:arm + (1 | patient.id),
-                         data = .x,
-                         prior = set_prior("uniform(-1,1)", lb = -1, ub = 1, class = "b")))
-
-# Save the model results
-summary_mods_high_40 <- map(mods_high_40,
-                            ~summary(.x))
-save(summary_mods_high_40,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-40_high-condition_modresults-", max(sims), ".RData"))
-
-rm(summary_mods_high_40)
-
-# Save the MCMC chains
-mcmc_mods_high_40 <- map(mods_high_40,
-                         ~mcmc_estimates_function(.x))
-save(mcmc_mods_high_40,
-     file = paste0("~/phase2b/data/bayes-generated/", Sys.Date(), "_simulated-lmm_intercept-only_lod-25_nk-40_high-condition_mcmc-", max(sims), ".RData"))
-
-rm(mods_high_40, mcmc_mods_high_40, nk_40)
