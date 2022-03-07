0881188c13b637b4a78a488c6b51812cb87c34d6
Suzanne Dufault <sdufault@dom-7g8ml7h-lt.ucsf.edu>
2022-03-07 09:16:09 -0800

    updating to run generally

--- analysis/03_advance-relapse.R
+++ analysis/03_advance-relapse.R
@@ -2,22 +2,13 @@ library(here)
 library(tidyverse)
 library(furrr)
 
-# load(here("data", "simulated-datasets", "2022-02-24_full-data-nk30-survival-97.RData"))
-
-even_relapse <- nk_30_outcome_1$nk_30_even
-high_relapse <- nk_30_outcome_1$nk_30_high
-highlow_relapse <- nk_30_outcome_1$nk_30_highlow
-rm(nk_30_outcome_1)
-
-dfsim <- even_relapse[[1]]$s90.situation1
-
 relapse_function <- function(dfsim){
   # date of interim analysis
   interim_date <- dfsim %>% 
     filter((week + enroll.day/7) == max(week + enroll.day/7)) %>% 
     mutate(interim.week = (week + # date of last visit
                              enroll.day/7 + #date of enrollment
-                             4)) # 25 days for culture conversion
+                             4 + 1)) # 25 days for culture conversion and 1 week for analysis
   
   temp <- dfsim %>% 
     dplyr::select(patient.id, eventtime, status, duration, regimen, enroll.day) %>% 
@@ -40,6 +31,11 @@ relapse_function <- function(dfsim){
     summarise(n_relapse_interim1 = sum(event.observed.interim1))
 }
 
+
+######################
+# S(t) = 0.9
+######################
+
 plan(multisession)
 t1.s90.situation1.even <- map(even_relapse, ~.x$s90.situation1) %>% 
   future_map_dfr(~relapse_function(.x),
@@ -60,8 +56,7 @@ s90.situation1.results <- bind_rows(t1.s90.situation1.even,
 
 save(s90.situation1.results,
      file = here("data","analyzed",
-                 paste0(Sys.Date(), "_s90-situation1-results_nk30", identifier, ".RData")))
-beepr::beep()
+                 paste0(Sys.Date(), "_s90-situation1-results_", identifier, ".RData")))
 
 
 plan(multisession)
@@ -84,8 +79,7 @@ s90.situation2.results <- bind_rows(t1.s90.situation2.even,
 
 save(s90.situation2.results,
      file = here("data","analyzed",
-                 paste0(Sys.Date(), "_s90-situation2-results_nk30", identifier, ".RData")))
-beepr::beep()
+                 paste0(Sys.Date(), "_s90-situation2-results_", identifier, ".RData")))
 
 
 plan(multisession)
@@ -108,5 +102,148 @@ s90.situation3.results <- bind_rows(t1.s90.situation3.even,
 
 save(s90.situation3.results,
      file = here("data","analyzed",
-                 paste0(Sys.Date(), "_s90-situation3-results_nk30", identifier, ".RData")))
-beepr::beep()
+                 paste0(Sys.Date(), "_s90-situation3-results_", identifier, ".RData")))
+
+######################
+# S(t) = 0.9
+######################
+
+plan(multisession)
+t1.s95.situation1.even <- map(even_relapse, ~.x$s95.situation1) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "even")
+t1.s95.situation1.high <- map(high_relapse, ~.x$s95.situation1) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "high")
+t1.s95.situation1.highlow <- map(highlow_relapse, ~.x$s95.situation1) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "highlow")
+
+s95.situation1.results <- bind_rows(t1.s95.situation1.even,
+                                    t1.s95.situation1.high,
+                                    t1.s95.situation1.highlow)
+
+save(s95.situation1.results,
+     file = here("data","analyzed",
+                 paste0(Sys.Date(), "_s95-situation1-results_", identifier, ".RData")))
+
+
+plan(multisession)
+t1.s95.situation2.even <- map(even_relapse, ~.x$s95.situation2) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "even")
+t1.s95.situation2.high <- map(high_relapse, ~.x$s95.situation2) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "high")
+t1.s95.situation2.highlow <- map(highlow_relapse, ~.x$s95.situation2) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "highlow")
+
+s95.situation2.results <- bind_rows(t1.s95.situation2.even,
+                                    t1.s95.situation2.high,
+                                    t1.s95.situation2.highlow)
+
+save(s95.situation2.results,
+     file = here("data","analyzed",
+                 paste0(Sys.Date(), "_s95-situation2-results_", identifier, ".RData")))
+
+
+plan(multisession)
+t1.s95.situation3.even <- map(even_relapse, ~.x$s95.situation3) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "even")
+t1.s95.situation3.high <- map(high_relapse, ~.x$s95.situation3) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "high")
+t1.s95.situation3.highlow <- map(highlow_relapse, ~.x$s95.situation3) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "highlow")
+
+s95.situation3.results <- bind_rows(t1.s95.situation3.even,
+                                    t1.s95.situation3.high,
+                                    t1.s95.situation3.highlow)
+
+save(s95.situation3.results,
+     file = here("data","analyzed",
+                 paste0(Sys.Date(), "_s95-situation3-results_", identifier, ".RData")))
+
+######################
+# S(t) = 0.97
+######################
+
+plan(multisession)
+t1.s97.situation1.even <- map(even_relapse, ~.x$s97.situation1) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "even")
+t1.s97.situation1.high <- map(high_relapse, ~.x$s97.situation1) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "high")
+t1.s97.situation1.highlow <- map(highlow_relapse, ~.x$s97.situation1) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "highlow")
+
+s97.situation1.results <- bind_rows(t1.s97.situation1.even,
+                                    t1.s97.situation1.high,
+                                    t1.s97.situation1.highlow)
+
+save(s97.situation1.results,
+     file = here("data","analyzed",
+                 paste0(Sys.Date(), "_s97-situation1-results_", identifier, ".RData")))
+
+
+plan(multisession)
+t1.s97.situation2.even <- map(even_relapse, ~.x$s97.situation2) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "even")
+t1.s97.situation2.high <- map(high_relapse, ~.x$s97.situation2) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "high")
+t1.s97.situation2.highlow <- map(highlow_relapse, ~.x$s97.situation2) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "highlow")
+
+s97.situation2.results <- bind_rows(t1.s97.situation2.even,
+                                    t1.s97.situation2.high,
+                                    t1.s97.situation2.highlow)
+
+save(s97.situation2.results,
+     file = here("data","analyzed",
+                 paste0(Sys.Date(), "_s97-situation2-results_", identifier, ".RData")))
+
+
+plan(multisession)
+t1.s97.situation3.even <- map(even_relapse, ~.x$s97.situation3) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "even")
+t1.s97.situation3.high <- map(high_relapse, ~.x$s97.situation3) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "high")
+t1.s97.situation3.highlow <- map(highlow_relapse, ~.x$s97.situation3) %>% 
+  future_map_dfr(~relapse_function(.x),
+                 .id = "sim") %>% 
+  mutate(ttp.condition = "highlow")
+
+s97.situation3.results <- bind_rows(t1.s97.situation3.even,
+                                    t1.s97.situation3.high,
+                                    t1.s97.situation3.highlow)
+
+save(s97.situation3.results,
+     file = here("data","analyzed",
+                 paste0(Sys.Date(), "_s97-situation3-results_", identifier, ".RData")))
