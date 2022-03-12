cd3d9b8690de6d07f86670a62979e87b698dbc8c
Suzanne Dufault <sdufault@dom-7g8ml7h-lt.ucsf.edu>
2022-03-11 19:36:37 -0800

    color palette

---
+++ lib/ucsf-color-palette.R
@@ -0,0 +1,9 @@
+ucsf_col_pal <- c("#052049", # UCSF Navy,
+                  "#178CCB", # B3 Blue,
+                  "#16A0AC", # C3 TEAL,
+                  "#32A03E", # D3 GREEN,
+                  "#6C62D0", # F3 PURPLE
+                  "#A238BA", # G3 VIOLET
+                  "#C42882", # H3 MAGENTA 
+                  "#FEB80A", # L3 YELLOW
+                  "#FA6E1E") # M3 ORANGE 
