46dd337aff19b636866a4724802680484ec6e8d5
Suzanne Dufault <sdufault15@gmail.com>
2022-06-07 17:07:51 -0700

    adding prior and model to this file for full consistency between scripts

--- munge/32_analyze-ttp-slope/30_job-file-for-running-on-cluster.R
+++ munge/32_analyze-ttp-slope/30_job-file-for-running-on-cluster.R
@@ -6,7 +6,10 @@
 
 library(here)
 
-sims <- c(1:200)
+sims <- c(1:100)
+model <- as.formula("yij_censored | cens(censored) ~ 1 + week + week:arm + (1 + week | patient.id)")
+priors <- set_prior("normal(0,4)", class = "b")
+  
 source(here("munge","32_analyze-ttp-slope","33_CLUSTER-brms-models-on-sim-data_even-condition.R"))
 source(here("munge","32_analyze-ttp-slope","34_CLUSTER-brms-models-on-sim-data_clustered-high-condition.R"))
 source(here("munge","32_analyze-ttp-slope","35_CLUSTER-brms-models-on-sim-data_clustered-highlow-condition.R"))
