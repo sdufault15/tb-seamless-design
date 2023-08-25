faab67e96de25416c551e865fe05b08991fa2a2e
Suzanne Dufault <sdufault@som-9v6qj02-dt.ucsf.edu>
2023-08-25 10:46:50 -0700

    updating header

--- munge/10_run-analysis-ttp.R
+++ munge/10_run-analysis-ttp.R
@@ -1,12 +1,12 @@
 ###################
 # Job for cluster
-# Bayes model breaks cluster after more than 100 runs, so separate scripts per hundred runs
+# Bayes model breaks cluster after more than a couple 100 runs, so separate scripts per ~hundred runs
 # 2023-06-28
 ###################
 
 library(here)
 library(brms)
-sims <- c(1:100)
+sims <- c(1:250)
 model <- as.formula("yij_censored | cens(censored) ~ 1 + week + week:arm + (1 + week | patient.id)")
 priors <- set_prior("normal(0,4)", class = "b")
 
